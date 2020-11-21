cb6aa1d34de8b507b7caf4f729d7158cb386db59
Anton <gerasin_03@mail.ru>
2020-11-21 21:12:44 +0300

    +

--- Graphing_Calculator/main.cpp
+++ Graphing_Calculator/main.cpp
@@ -6,16 +6,11 @@
 #include <QtWidgets/QApplication>
 
 
-
 int main(int argc, char *argv[]) {
-
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(NULL);
-    std::cout << 8;
-
     QApplication app(argc, argv);
     MyWindow *mywindow = new MyWindow;
-    
     
     return app.exec();
 }
